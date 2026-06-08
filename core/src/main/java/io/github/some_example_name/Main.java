f10beb04653cea379115babcb196633c5f76d719
ExtraWingsPlease <20pe.j@isaacnewtonsixthform.co.uk>
2026-06-08 15:09:14 +0100

    .

--- core/src/main/java/io/github/some_example_name/Main.java
+++ core/src/main/java/io/github/some_example_name/Main.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.math.Vector;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
@@ -16,22 +17,39 @@ import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main implements ApplicationListener {
     PerspectiveCamera camera;
     Vector3 vertical;
     boolean locked;
+    ModelInstance sphereInstance;
+    ModelBuilder modelBuilder;
+    ModelBatch modelBatch;
+    Model model;
+    Vector3 camPosition;
+    Vector3 camDirection;
 
     @Override
     public void create() {
+        modelBatch = new ModelBatch();
         camera = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        camera.position.set(0,0,0);
-        camera.lookAt(1,0,0);
+        camPosition = new Vector3(10,10,10);
+        camera.position.set(camPosition);
+
+        camera.lookAt(0,0,0);
+        camDirection = camera.direction.cpy().nor();
+        camera.near = 1f;
+        camera.far = 300f;
         camera.update();
         vertical = new Vector3(0,0,1);
         boolean locked = false;
-
+        modelBuilder = new ModelBuilder();
+        model = modelBuilder.createBox(5f, 5f, 5f,
+            new Material(ColorAttribute.createDiffuse(Color.GREEN)),
+            Usage.Position | Usage.Normal);
+        sphereInstance = new ModelInstance(model);
 
     }
 
@@ -46,29 +64,41 @@ public class Main implements ApplicationListener {
 
     public void doCameraMovement(PerspectiveCamera camera, float speed, float sensitivity, boolean locked){
         if(Gdx.input.isKeyPressed(Input.Keys.W)){
-            camera.position.add(camera.direction.cpy().scl(speed));
+            camPosition.add(camDirection.cpy().nor().scl(speed));
         }
         if(Gdx.input.isKeyPressed(Input.Keys.S)){
-            camera.position.sub(camera.direction.cpy().scl(speed));
+            camPosition.sub(camDirection.cpy().nor().scl(speed));
         }
         if(Gdx.input.isKeyPressed(Input.Keys.D)){
-            camera.position.add(camera.direction.cpy().crs(0,0,1).scl(speed));
+            camPosition.add(camDirection.cpy().nor().crs(0,0,1).scl(speed));
         }
         if(Gdx.input.isKeyPressed(Input.Keys.A)){
-            camera.position.sub(camera.direction.cpy().crs(0,0,1).scl(speed));
+            camPosition.sub(camDirection.cpy().nor().crs(0,0,1).scl(speed));
         }
         if(locked){
-            camera.direction.rotate(camera.direction.cpy().crs(0,0,1), sensitivity * (Gdx.input.getY() - ((float) Gdx.graphics.getHeight() /2));
-            camera.direction.rotate(vertical, sensitivity * (Gdx.input.getX() - ((float) Gdx.graphics.getWidth() /2));
+            camDirection.rotate(camDirection.cpy().nor().crs(0,0,1), sensitivity * (Gdx.input.getY() - ((float) Gdx.graphics.getHeight() /2)));
+            camDirection.rotate(vertical, sensitivity * (Gdx.input.getX() - ((float) Gdx.graphics.getWidth() /2)));
             Gdx.input.setCursorPosition(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
         }
 
+        camera.position.set(camPosition);
+        camera.direction.set(camDirection);
+        System.out.println(camera.position);
+;
     }
     @Override
     public void render() {
+        ScreenUtils.clear(0f,0f,0f,0f);
         if(Gdx.input.isKeyJustPressed(Input.Keys.CONTROL_LEFT)){
             locked = !locked;
         }
+        doCameraMovement(camera,1,1,locked);
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+        modelBatch.begin(camera);
+        camera.update();
+        modelBatch.render(sphereInstance);
+        modelBatch.end();
     }
 
     @Override
@@ -84,5 +114,7 @@ public class Main implements ApplicationListener {
     @Override
     public void dispose() {
         // Destroy application's resources here.
+        modelBatch.dispose();
+        model.dispose();
     }
 }
